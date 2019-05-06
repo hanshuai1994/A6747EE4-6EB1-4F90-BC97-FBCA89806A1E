bb36cafa3d7559e0397fcf2fffaf6e458f618f57
Andrea0725 <40812876+Andrea0725@users.noreply.github.com>
2019-05-06 16:07:03 +0800

    上传后台数据接口（新增、删除、修改、查询）
    上传后台数据接口（新增、删除、修改、查询）

---
+++ js/yunweiData.js
@@ -0,0 +1,79 @@
+let Http;
+const server = "aliyun_win";
+
+switch (server) {
+    case "localhost":
+        Http = "http://localhost:8080/buildingManagement/"; //本地
+        break;
+    case "aliyun_win":
+        Http = "http://121.40.174.117:8080/buildingManagement/"; //aliyun win
+        break;
+    default:
+        break;
+}
+
+//新增运维数据
+function addYunweiData(build, floor, room, title, time, state, content){
+	$.ajax({
+	    type: "POST",
+	    url: Http + 'addYunweiData.do',
+	    ansyc: false,
+	    data: {build: build, floor: floor, room: room, title: title, time: time, state: state, content: content},
+	    dataType: "json",
+	    success: function(data) {
+	        resp = eval(data);
+	        if(resp[0].data == 100){
+	        	console.log("运维数据成功插入数据库！");
+	        }
+	    }
+	})
+}
+
+//根据id删除对应运维数据
+function deleteYunweiData(id){
+	$.ajax({
+	    type: "POST",
+	    url: Http + 'deleteYunweiData.do',
+	    ansyc: false,
+	    data: {id: id},
+	    dataType: "json",
+	    success: function(data) {
+	        resp = eval(data);
+	        if(resp[0].data == 100){
+	        	console.log("当前运维数据删除成功！");
+	        }
+	    }
+	})
+}
+
+//根据id修改运维数据
+function updateYunweiData(id, newBuild, newFloor, newRoom, newTitle, newTime, newState, newContent){
+	$.ajax({
+	    type: "POST",
+	    url: Http + 'updateYunweiData.do',
+	    ansyc: false,
+	    data: {id: id, newBuild: newBuild, newFloor: newFloor, newRoom: newRoom, newTitle: newTitle, newTime: newTime, newState: newState, newContent: newContent},
+	    dataType: "json",
+	    success: function(data) {
+	        resp = eval(data);
+	        if(resp[0].data == 100){
+	        	console.log("当前运维数据修改成功！");
+	        }
+	    }
+	})
+}
+
+//查询所有运维数据
+function selectAllYunweiData(){
+	$.ajax({
+	    type: "POST",
+	    url: Http + 'selectAllYunweiData.do',
+	    ansyc: false,
+	    data: {},
+	    dataType: "json",
+	    success: function(data) {
+	        resp = eval(data);
+	        console.log('所有运维数据', resp);
+	    }
+	})
+}
